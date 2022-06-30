4edcbbd1558429822bd99d439e08283e183825f4
Ian Miller <theiandmiller@gmail.com>
2022-06-30 05:37:30 -0500

    api

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab";
 
 import useStyles from "./styles";
@@ -15,7 +15,7 @@ const Map = () => {
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{ key: "AIzaSyDhqj-xzcSPvUBy3qcJM1FIwt7dANuKRiY" }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
